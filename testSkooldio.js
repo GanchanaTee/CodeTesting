b84be4bb1f197653ac484a5204c635d3008fc34f
ganchana youpaisan <ganchana78@gmail.com>
2022-04-23 19:42:15 +0700

    Code Test 1

---
+++ testSkooldio.js
@@ -0,0 +1,31 @@
+function isEqual(value, expectedValue) {
+    if (value === expectedValue) {
+      console.log('Test Pass');
+      return;
+    };
+    console.error('Expected', JSON.stringify(expectedValue), 'got', JSON.stringify(value))
+  }
+  
+  /**
+   * Encode Text
+   * Return - String that encoded
+   */
+  function encode(input) {
+      let splitInput = input.split('');
+      let j = 1;
+      for (let i = 0; i < splitInput.length ; i ++) {    
+        if(j >=2) {
+          splitInput.splice(i-1,1);
+          i--; 
+        }
+        if(splitInput[i] !== splitInput[i+1] ) {
+          splitInput.splice(i+1,0,j);
+          j=0;
+          i++;
+        }
+        j++;
+      }
+      return splitInput.join('');
+  }
+
+  isEqual(encode('schools'), 's1c1h1o2l1s1');
